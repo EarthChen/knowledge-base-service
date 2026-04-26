321fc7f984bc93cbfaa902f332f07bd5107af15a
earthchen <earthchen@apache.org>
2026-04-26 11:26:52 +0800

    feat(wiki): export tree builder and new models from wiki package

--- wiki/__init__.py
+++ wiki/__init__.py
@@ -1,1 +1,10 @@
 """Wiki generation module for KBS."""
+
+from wiki.models import WikiSpaceNode, WikiSectionNode
+from wiki.tree_builder import WikiTreeBuilder
+
+__all__ = [
+    "WikiSpaceNode",
+    "WikiSectionNode",
+    "WikiTreeBuilder",
+]

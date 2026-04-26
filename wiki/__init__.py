fba3c315031f1f4beaf3879da12197725a85a555
earthchen <earthchen@apache.org>
2026-04-26 12:25:49 +0800

    feat(wiki): export Phase 2 RAG components and complete integration validation

--- wiki/__init__.py
+++ wiki/__init__.py
@@ -1,12 +1,17 @@
 """Wiki generation module for KBS."""
 
+from wiki.chunk_indexer import CodeChunkIndexer
+from wiki.chunk_retriever import ChunkRetriever
 from wiki.importance_scorer import ImportanceScorer
-from wiki.models import CodeSnippet, ImportanceTier, WikiSectionNode, WikiSpaceNode
+from wiki.models import ChunkSnippet, CodeSnippet, ImportanceTier, WikiSectionNode, WikiSpaceNode
 from wiki.source_code_reader import SourceCodeReader
 from wiki.tree_builder import WikiTreeBuilder
 
 __all__ = [
+    "ChunkSnippet",
+    "CodeChunkIndexer",
     "CodeSnippet",
+    "ChunkRetriever",
     "ImportanceScorer",
     "ImportanceTier",
     "SourceCodeReader",
